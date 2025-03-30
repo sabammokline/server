2f7005977286fc93c96467a64a2281e9fc2d8cc1
Saba <moklina1222@gmail.com>
2025-03-30 04:59:26 +0100

    addedaccountviewandserializer

--- serverprojet/asgi.py
+++ serverprojet/asgi.py
@@ -1,16 +1,13 @@
-"""
-ASGI config for serverprojet project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
+# healthapp/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serverprojet.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Add your WebSocket routing here if needed
+})
+AUTH_USER_MODEL = 'accounts.CustomUser'
